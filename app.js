8a797a8c348c8d43d082b383153be0b615fe638e
Ahmad Abdeen <ahmad2013hamdi@gmail.com>
2021-06-20 17:06:43 +0200

    Feat : added the event listeners for Enter and Click

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var UIController = (function(){
 
 
+    
+
 
 })();
 
@@ -12,6 +14,22 @@ var budgetController = (function(){
 
 var controller = (function(UICtrl,budgetCtrl){
 
+    var addItem = function(){
+
+        // get the inout data
+        //add the item to budget controller 
+        //add the item to ui 
+        // calculate the budget 
+        // display budget on UI 
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click',addItem);
     
+    document.addEventListener('keypress',function(event){
+        if(event.key=='Enter'){
+            addItem();
+        };
+    });
 
 })(UIController,budgetController);
