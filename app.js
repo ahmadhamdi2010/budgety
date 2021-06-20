03a17aac0350c44866a05e1f25d3073c3ca832de
Ahmad Abdeen <ahmad2013hamdi@gmail.com>
2021-06-20 17:27:42 +0200

    Feat : added a DOM string object to save all class names, and added a get input Function

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var UIController = (function(){
 
+    var DOMStrings = {
+        typeField : '.add__type',
+        descriptionField : '.add__description',
+        valueField : '.add__value',
+        addBtn : '.add__btn'
+    }
 
+    return {
+        getInput : function(){
+
+            return {
+                type : document.querySelector(DOMStrings.typeField).value,
+                description : document.querySelector(DOMStrings.descriptionField).value,
+                value : document.querySelector(DOMStrings.valueField).value
+            }
+            
     
+        },
+        getDOMStrings : function(){
+            return DOMStrings;
+        }
+    };
 
 
 })();
@@ -14,9 +34,14 @@ var budgetController = (function(){
 
 var controller = (function(UICtrl,budgetCtrl){
 
+    var DOM = UICtrl.getDOMStrings();
     var addItem = function(){
 
         // get the inout data
+
+        var inputData = UICtrl.getInput();
+        console.log(inputData);
+
         //add the item to budget controller 
         //add the item to ui 
         // calculate the budget 
@@ -24,7 +49,7 @@ var controller = (function(UICtrl,budgetCtrl){
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click',addItem);
+    document.querySelector(DOM.addBtn).addEventListener('click',addItem);
     
     document.addEventListener('keypress',function(event){
         if(event.key=='Enter'){
